863e4058be373afc8afea14a544fba3a69724b74
Guilain <guilain.mbayo@cpnv.ch>
2020-10-30 09:05:57 +0100

    Dépose les fichiers de base

---
+++ montantEnVaudois.cpp
@@ -0,0 +1,40 @@
+//
+// Created by maany on 29.10.2020.
+//
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+string montantEnVaudois(long double montant){
+    string resultat = " test ";
+
+    //Control du nombre entré par l'utilisateur
+
+    //Appel de La fct Separateur par triplet
+
+    // etc...
+
+    return resultat;
+}
+
+//fct 1
+/* nom: SeparateurParTriplets
+ * but: Reçoit un nombre entre 0 et 999999999999999.99 et le décompose par triplets de 10 puissance 3.
+ *      Renvoie chaque triplet dans des variables déclarées dans la fonction montantEnVaudois.
+ * paramètres: nb1, nb2, nb3, nb4, nb5, nb6
+ */
+
+//fct 2
+/* nom: SeparateurEnChiffre
+ * but: Reçoit un triplet de chiffres et le sépart en chiffre seule.
+ * paramètre: int triplet
+ */
+
+//fct 3
+/* nom: convertisseurChiffreEnLettre
+ * but: Prend chaque chiffre reçu et retourne sa version écrite en tenant compte de s'il sagit d'une
+ *      unité, dizaine, etc.. grâce à un flag.
+ * paramètre: int chiffre, flag
+ */
