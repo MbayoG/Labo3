a08db6ebc04e6652adda8d89352d0fa6b5b63726
Guilain <guilain.mbayo@cpnv.ch>
2020-11-03 13:58:40 +0100

    Modifie le fichier .gitignore

--- montantEnVaudois.cpp
+++ montantEnVaudois.cpp
@@ -9,41 +9,22 @@
 using namespace std;
 
 //fct 1
-/* nom: SeparateurParTriplets
+/* nom: separateurParTriplets
  * but: Reçoit un nombre entre 0 et 999999999999999.99 et le décompose par triplets de 10 puissance 3.
  *      Renvoie chaque triplet dans des variables déclarées dans la fonction montantEnVaudois.
  * paramètres: long double montant, int& nb1, int& nb2, int& nb3, int& nb4, int& nb5, int& nb6
  */
-void SeparateurParTriplets(long double montant, int& nb1, int& nb2, int& nb3, int& nb4, int& nb5, int& nb6){
+void separateurParTriplets(long double montant, int& nb1, int& nb2, int& nb3, int& nb4, int& nb5, int& nb6){
 	long long montantEnLong = floor(montant);
 	nb6 = (montant - floor(montant))*100; //modifier l'arrondi pour les centimes
 
-	nb5 = montantEnLong % 1000;
-	nb4 = montantEnLong/1000 %1000;
-	nb3 = montantEnLong/1000000 %1000;
+	nb5 = montantEnLong % 1000;	//centaine
+	nb4 = montantEnLong/1000 %1000;	//millier
+	nb3 = montantEnLong/1000000 %1000;	//etc...
 	nb2 = montantEnLong/1000000000 %1000;
-	nb1 = montantEnLong/1000000000000 %1000;	//Il pourrait ^tre intéressant de trouver une manière plus élégante de faire ça.
+	nb1 = montantEnLong/1000000000000 %1000;	//Il pourrait être intéressant de trouver une manière plus élégante de faire ça.
 
 }
-
-string montantEnVaudois(long double montant){
-    string resultat = " test ";
-    int nb1, nb2, nb3, nb4, nb5, nb6;
-
-    //Appel de La fct Separateur par triplet
-    SeparateurParTriplets(montant, nb1,nb2,nb3,nb4,nb5,nb6);
-    // etc...
-
-    return resultat;
-}
-
-
-
-//fct 2
-/* nom: SeparateurEnChiffre
- * but: Reçoit un triplet de chiffres et le sépart en chiffre seule.
- * paramètre: int triplet
- */
 
 //fct 3
 /* nom: convertisseurChiffreEnLettre
@@ -51,3 +32,34 @@ string montantEnVaudois(long double montant){
  *      unité, dizaine, etc.. grâce à un flag.
  * paramètre: int chiffre, flag
  */
+string convertisseurChiffreEnLettres(int chiffre){
+	string resultat = "";
+
+	return resultat;
+}
+
+//fct 2
+/* nom: separateurEnChiffre
+ * but: Reçoit un triplet de chiffres et le sépart en chiffre seule.
+ * paramètre: int triplet
+ */
+string separateurEnChiffres(int triplet){
+	string resultat = "";
+
+	for(int i = 1; i <= 100; i *= 10){
+		resultat += convertisseurChiffreEnLettres(triplet/i%10);
+	}
+}
+
+string montantEnVaudois(long double montant){
+    string resultat = " test ";
+    int nb1, nb2, nb3, nb4, nb5, nb6;
+
+    //Appel de La fct SeparateurParTriplet
+    separateurParTriplets(montant, nb1,nb2,nb3,nb4,nb5,nb6);
+    // etc...
+
+
+    return resultat;
+}
+
