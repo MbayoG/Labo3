91ad1dc0ebc09a02241a7445e54ea6a05011b1ff
Felipe <fefe8439@hotmail.com>
2020-11-10 15:40:50 +0100

    suppression saisie + Correction pour le Code check

--- montantEnVaudois.cpp
+++ montantEnVaudois.cpp
@@ -5,27 +5,9 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <limits>
 
 using namespace std;
-
-//fct qui check le chiffre rentré par l'utilisateur
-void obtenirunbonNombre(long double &montant) {
-    bool premier_saisie = true;
-    do {
-        cin.clear();
-        if (!premier_saisie) {
-            //cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cin >> montant; // finir par EOF (Ctrl-D) ou autre chose qu'un nombre
-            cout << fixed << setprecision(2) << montant << endl; // arrondi different de celui de la fonction demandee
-        }
-        if (montant < 0) {
-            cout << "erreur : montant negatif" << endl;
-        } if (montant >= 1000000000000000) {
-            cout << "erreur : montant trop grand" << endl;
-        }
-        premier_saisie = false;
-    } while (!(montant >= 0 && montant <= 999999999999999.99));
-}
 
 //fct 1
 /* nom: separateurParTriplets
@@ -33,26 +15,27 @@ void obtenirunbonNombre(long double &montant) {
  *      Renvoie chaque triplet dans des variables déclarées dans la fonction montantEnVaudois.
  * paramètres: long double montant, int& nb1, int& nb2, int& nb3, int& nb4, int& nb5, int& nb6
  */
-void separateurParTriplets(long double& montant, int &nb1, int &nb2, int &nb3, int &nb4, int &nb5, int &nb6) {
+void separateurParTriplets(long double &montant, int &nb1, int &nb2, int &nb3, int &nb4, int &nb5, int &nb6) {
     long long montantEnLong = floor(montant);
     nb6 = (montant - floor(montant)) * 1000; //modifier l'arrondi pour les centimes
-    if(nb6 % 10 >= 5){
-    	nb6 = nb6/10 +1;
-    }else{
-    	nb6 = nb6 / 10;
+    if (nb6 % 10 >= 5) {
+        nb6 = nb6 / 10 + 1;
+    } else {
+        nb6 = nb6 / 10;
     }
 
-	 if(nb6 >= 100){
-		montant += nb6/100;
-		montantEnLong += nb6/100;
-		nb6 = nb6%100;
-	 }
+    if (nb6 >= 100) {
+        montant += nb6 / 100;
+        montantEnLong += nb6 / 100;
+        nb6 = nb6 % 100;
+    }
 
     nb5 = montantEnLong % 1000;    //centaine
     nb4 = montantEnLong / 1000 % 1000;    //millier
     nb3 = montantEnLong / 1000000 % 1000;    //etc...
     nb2 = montantEnLong / 1000000000 % 1000;
-    nb1 = montantEnLong / 1000000000000 % 1000;    //Il pourrait être intéressant de trouver une manière plus élégante de faire ça.
+    nb1 = montantEnLong / 1000000000000 %
+          1000;    //Il pourrait être intéressant de trouver une manière plus élégante de faire ça.
 
 
 }
@@ -106,47 +89,45 @@ string convertisseurChiffreEnLettres(int triplet, bool flagcentaine) {
         default:
             resultat += "";
     }
-    if(centaine && flagcentaine && !dizaine && !unite && centaine !=1){
-    	resultat += "cents";
-    } else if(centaine){
-    	resultat += "cent";
+    if (centaine && flagcentaine && !dizaine && !unite && centaine != 1) {
+        resultat += "cents";
+    } else if (centaine) {
+        resultat += "cent";
     }
-    if((dizaine || unite) && centaine){
-		 resultat += "-";
+    if ((dizaine || unite) && centaine) {
+        resultat += "-";
     }
 
     switch (dizaine) //Ecriture des dizaines
     {
         case 1:
-           if (unite >= 1 && unite <= 6)
-           {
-              switch (unite)
-              {
-                 case 1:
-                    resultat += "onze";
-                    break;
-                 case 2:
-                    resultat += "douze";
-                    break;
-                 case 3:
-                    resultat += "treize";
-                    break;
-                 case 4:
-                    resultat += "quatorze";
-                    break;
-                 case 5:
-                    resultat += "quinze";
-                    break;
-                 case 6:
-                    resultat += "seize";
-                    break;
-                 default:
-                    resultat += "";
-              }
-           } else{
-              resultat += "dix";
-           }
-          break;
+            if (unite >= 1 && unite <= 6) {
+                switch (unite) {
+                    case 1:
+                        resultat += "onze";
+                        break;
+                    case 2:
+                        resultat += "douze";
+                        break;
+                    case 3:
+                        resultat += "treize";
+                        break;
+                    case 4:
+                        resultat += "quatorze";
+                        break;
+                    case 5:
+                        resultat += "quinze";
+                        break;
+                    case 6:
+                        resultat += "seize";
+                        break;
+                    default:
+                        resultat += "";
+                }
+            } else {
+                resultat += "dix";
+            }
+            break;
         case 2:
             resultat += "vingt";
             break;
@@ -178,58 +159,59 @@ string convertisseurChiffreEnLettres(int triplet, bool flagcentaine) {
             resultat += "";
     }
 
-    if(dizaine > 1 && unite == 1){
-    	resultat += "-et";
+    if (dizaine > 1 && unite == 1) {
+        resultat += "-et";
     }
     if (dizaine != 1 || unite > 6) {
-    	if((dizaine || centaine) && unite){
-    		resultat += "-";
-    	}
-       switch (unite) //Ecriture des unitees
-       {
-          case 1:
-             resultat += "un";
-             break;
-          case 2:
-             resultat += "deux";
-             break;
-          case 3:
-             resultat += "trois";
-             break;
-          case 4:
-             resultat += "quatre";
-             break;
-          case 5:
-             resultat += "cinq";
-             break;
-          case 6:
-             resultat += "six";
-             break;
-          case 7:
-             resultat += "sept";
-             break;
-          case 8:
-             resultat += "huit";
-             break;
-          case 9:
-             resultat += "neuf";
-             break;
-          case 0:
-             resultat += "";
-             break;
-          default:
-             resultat += "";
-       }
+        if ((dizaine || centaine) && unite) {
+            resultat += "-";
+        }
+        switch (unite) //Ecriture des unitees
+        {
+            case 1:
+                resultat += "un";
+                break;
+            case 2:
+                resultat += "deux";
+                break;
+            case 3:
+                resultat += "trois";
+                break;
+            case 4:
+                resultat += "quatre";
+                break;
+            case 5:
+                resultat += "cinq";
+                break;
+            case 6:
+                resultat += "six";
+                break;
+            case 7:
+                resultat += "sept";
+                break;
+            case 8:
+                resultat += "huit";
+                break;
+            case 9:
+                resultat += "neuf";
+                break;
+            case 0:
+                resultat += "";
+                break;
+            default:
+                resultat += "";
+        }
     }
     return resultat;
 }
 
 string montantEnVaudois(long double montant) {
+    if (montant < 0) return "erreur : montant negatif";
+    if (montant > 999999999999999.99) return "erreur : montant trop grand";
     string resultat, unite;
     int nb1 = 0, nb2 = 0, nb3 = 0, nb4 = 0, nb5 = 0, nb6 = 0;
     bool flagcentaine = true;
-    //Appel de la fct
-    obtenirunbonNombre(montant);
+
     //Appel de La fct SeparateurParTriplet
     separateurParTriplets(montant, nb1, nb2, nb3, nb4, nb5, nb6);
     // etc...
@@ -238,28 +220,28 @@ string montantEnVaudois(long double montant) {
 
 
     //transformer ca en fonction pour éviter la répetitions !!!
-    if(floor(montant) == 1){
-		 unite = " franc";
-    } else if(!floor(montant)){
-    	unite = "";
-    }else{
-		 unite = " francs"; // Pour changer en euros facilement  PS : laisser un espace avant l'unité merci ^^
+    if (floor(montant) == 1) {
+        unite = " franc";
+    } else if (!floor(montant)) {
+        unite = "";
+    } else {
+        unite = " francs"; // Pour changer en euros facilement  PS : laisser un espace avant l'unité merci ^^
     }
-    if((!montant)){
-		 unite = "zero franc";
+    if ((!montant)) {
+        unite = "zero franc";
     }
 
     resultat += convertisseurChiffreEnLettres(nb1, flagcentaine);
     if (nb1 == 0) {
         resultat += "";
     } else {
-       if(nb1 == 1){
-         resultat += " billion";
-       }else{
-          resultat += "-billions";
-       }
-        if(nb2 || nb3 || nb4 || nb5){
-        	resultat += "-";
+        if (nb1 == 1) {
+            resultat += " billion";
+        } else {
+            resultat += "-billions";
+        }
+        if (nb2 || nb3 || nb4 || nb5) {
+            resultat += "-";
         }
     }
     resultat += convertisseurChiffreEnLettres(nb2, flagcentaine);
@@ -267,65 +249,63 @@ string montantEnVaudois(long double montant) {
         resultat += "";
     } else {
         resultat += "-milliards";
-		 if(nb3 || nb4 || nb5){
-			 resultat += "-";
-		 }
+        if (nb3 || nb4 || nb5) {
+            resultat += "-";
+        }
     }
     resultat += convertisseurChiffreEnLettres(nb3, flagcentaine);
     if (nb3 == 0) {
         resultat += "";
     } else {
         resultat += "-millions";
-		 if(nb4 || nb5){
-			 resultat += "-";
-		 }
+        if (nb4 || nb5) {
+            resultat += "-";
+        }
     }
     flagcentaine = false;
-    if(nb4 == 1){
-    	if(!nb1 && !nb2 && !nb3){
-    		resultat += "-";
-    	}
-    	resultat += "mille";
-		 if(nb5){
-			 resultat += "-";
-		 }
-    }
-    else{
-		 resultat += convertisseurChiffreEnLettres(nb4, flagcentaine);
-		 if (nb4 == 0) {
-			 resultat += "";
-		 } else {
-			 resultat += "-mille";
-			 if(nb5){
-				 resultat += "-";
-			 }
-		 }
+    if (nb4 == 1) {
+        if (!nb1 && !nb2 && !nb3) {
+            resultat += "-";
+        }
+        resultat += "mille";
+        if (nb5) {
+            resultat += "-";
+        }
+    } else {
+        resultat += convertisseurChiffreEnLettres(nb4, flagcentaine);
+        if (nb4 == 0) {
+            resultat += "";
+        } else {
+            resultat += "-mille";
+            if (nb5) {
+                resultat += "-";
+            }
+        }
     }
 
-	flagcentaine = true;
+    flagcentaine = true;
     resultat += convertisseurChiffreEnLettres(nb5, flagcentaine);
 
     //cas exception
-   if((nb1 || nb2 || nb3) && nb4 == 0 && nb5 == 0)
-   {
-      resultat += " de";
-   }
+    if ((nb1 || nb2 || nb3) && nb4 == 0 && nb5 == 0) {
+        resultat += " de";
+    }
 
     resultat += unite;
     if (nb6 == 0 || !floor(montant)) {
         resultat += "";
-    } else{
+    } else {
         resultat += " et ";
     }
     resultat += convertisseurChiffreEnLettres(nb6, flagcentaine);
     if (nb6 == 0) {
         resultat += "";
     } else {
-    	if(nb6 == 1){
-    		resultat += " centime";
-    	}else{
-			resultat += " centimes";
-    	}
+        if (nb6 == 1) {
+            resultat += " centime";
+        } else {
+            resultat += " centimes";
+        }
     }
 
     return resultat;
